a1a0dc3c3ef794bd8bbedbb6cc50c4305ff4d7f1
HeatherDa <crystal_tree@hotmail.com>
2016-03-03 00:24:38 -0600

    It works, just need to print results

---
+++ Pet.java
@@ -0,0 +1,59 @@
+package com.Heather;
+
+import java.util.*;
+
+/**
+ * Created by cryst on 3/2/2016.
+ */
+public class Pet {
+    private String name;
+    private String type;
+    private HashMap<String, Integer>vperday=new HashMap<>();//holds number of visits per day to a pet
+    private HashMap<String, Integer>tvperday=new HashMap<>();//holds number of pets per day
+    private ArrayList<String> days=new ArrayList<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+    private int visits;
+    private String address;
+
+
+    Pet(String petName, String petType, String petAddress){
+        this.name=petName;
+        this.type=petType;
+        this.vperday=new HashMap<>();
+        this.visits=0;
+        this.address=petAddress;
+    }
+
+    public void addDay(){ //track visits per day to a pet
+        Scanner scanint=new Scanner(System.in);
+        for(String aday:days) {
+            System.out.println("How many visits are needed for " + this.name + " on " + aday);//elicit information about visits
+            int visits=scanint.nextInt();
+            if ((tvperday.containsKey(aday)) && (tvperday.get(aday) < 7)) {//if another pet is being visited on the same day
+                this.vperday.put(aday, visits); //update the information for this day
+                tvperday.put(aday, (tvperday.get(aday) + 1));//add a visit to the total for this day
+            }
+            else if ((tvperday.containsKey(aday))&&(tvperday.get(aday)>=6)){
+                System.out.println(aday+" is already full.");
+            }else {
+                this.vperday.put(aday, visits);//update information for this pet on this day
+                tvperday.put(aday,1);//add this day to hash if there is no entry
+            }
+        }
+    }
+    public String writePetInfo(int v){
+        /*System.out.println("\nPet's name: "+this.name);
+        System.out.println("Address: "+this.address);*/
+       // for (String d:this.vperday.keySet()){
+            String entry="Visit "+this.name+" the "+this.type+" "+v+" times at "+this.address+".";
+
+        //}
+        return entry;
+    }
+
+
+
+
+    public HashMap<String, Integer> getvperday() {
+        return this.vperday;
+    }
+}
---
+++ PetManager.java
@@ -0,0 +1,48 @@
+package com.Heather;
+import java.util.*;
+
+public class PetManager {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan=new Scanner(System.in);
+        Scanner scani=new Scanner(System.in);
+        ArrayList<Pet> petlist=new ArrayList<>();
+        ArrayList<String> daylist=new ArrayList<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+        HashMap<String, ArrayList<String>> printday=new HashMap<>();
+        System.out.println("How many pets need to be added to the schedule?");
+        int numpet=scani.nextInt();
+        for(int i=0; i<numpet; i++) {
+            System.out.println("What is the name of the pet?");
+            String name = scan.nextLine();
+            System.out.println("What kind of animal is it?");
+            String type = scan.nextLine();
+            System.out.println("What is the address where the animal lives?");
+            String address = scan.nextLine();
+            Pet n=new Pet(name, type, address);
+            petlist.add(n);
+        }
+        for(Pet name:petlist){//go through once per pet object
+            name.addDay();
+            HashMap<String, Integer>sch=name.getvperday();//get schedule for this pet
+            for (String d:daylist){ //get a day of the week
+                if (sch.containsKey(d)){//if that day is applicable to this pet
+                    ArrayList<String>val=new ArrayList<>();
+                    if (printday.containsKey(d)){//if the day is in printday already
+                        val=printday.get(d);//get arraylist of strings to print for this day
+                        val.add(name.writePetInfo(sch.get(d)));//add string with visit schedule for this pet on this day
+                        printday.put(d,val);
+
+                    }else{//if the day is not in the print schedule yet
+                        val.add(name.writePetInfo(sch.get(d)));
+                        printday.put(d,val);
+
+                    }
+
+                }
+            }
+        }
+
+
+    }
+}
